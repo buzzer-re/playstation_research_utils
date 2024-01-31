1e95274f9bc6171fb9cf81dcd767bce44db0bcea
buzzer-re <22428720+buzzer-re@users.noreply.github.com>
2024-01-31 03:52:41 -0300

    adds sprx2dll script

---
+++ sprx2dll/README.md
@@ -0,0 +1,51 @@
+# SPRX to DLL
+
+Some SPRX (Signed Playstation Relocatable eXecutable) files inside the PlayStation 5, are written using the [Mono](https://www.mono-project.com/) .Net framework, so the code is actually generated inside a DLL file and loaded by the ELF at runtime.
+
+This script automatize the DLL extraction from this executables by parsing the ELF symbols until find a specific NID, (N)ame(ID)entifiers, from the `dll_start` symbol, and them extracting the DLL inside the ELF file.
+
+
+## Install
+
+In order to use, you just need to install the dependencies with `pipenv`.
+
+```
+pip install pipenv
+git clone git@github.com:buzzer-re/ps_scripts.git
+cd ps_scripts/sprx2dll
+pipenv shell && pipenv sync
+```
+## Usage
+
+```
+$ python sprx2dll.py -h
+usage: Extract embedded DLL files from PS5 SPRX (Signed Shared Library) files
+
+positional arguments:
+  SPRX
+
+options:
+  -h, --help            show this help message and exit
+  --output OUTPUT, -o OUTPUT
+                        Output filename, default: filename.dl
+```
+
+## Example
+
+```
+$ python sprx2dll.py Sce.Vsh.Theme.dll.sprx
+[+] Loading Sce.Vsh.Theme.dll.sprx... [+]
+[+] Found dll_start symbol, extracting PE... [+]
+[+] PE is at virtual address 0x34050, extracting... [+]
+[+] Saving as Sce.Vsh.Theme.dll [+]
+[+] Done! [+]
+```
+
+
+In order to get the executables, you need to decrypt the executables from your OWN console, most .Net programs are are located at `/system_ex/common_ex/lib`
+
+
+## Resources
+
+- [SELF-SPRX](https://psdevwiki.com/ps3/SELF_-_SPRX)
+- [NID](https://www.youtube.com/watch?v=xxKNxdulGq0)
---
+++ sprx2dll/sprx2dll.py
@@ -0,0 +1,65 @@
+import lief
+import argparse
+import os, sys
+
+
+def log_error(msg: str):
+    print(f"[-] {msg} [-]")
+
+def log(msg: str):
+    print(f"[+] {msg} [+]") 
+
+
+# This is the dll_start GOT symbol NID
+DLL_START_NID = "Tkla3v2SbEs#C#A" 
+
+if __name__ == '__main__':
+    lief.logging.set_level(lief.logging.LOGGING_LEVEL.CRITICAL) # Disable warning
+
+    parser = argparse.ArgumentParser("sprx2dll", "Extract embedded DLL files from PS5 SPRX (Signed Shared Library) files")
+    parser.add_argument("SPRX")
+    parser.add_argument("--output", "-o", help="Output filename, default: filename.dll", default='')
+    
+    args = parser.parse_args()
+
+    target = args.SPRX
+    output = args.output
+
+    if not os.path.exists(target):
+        log_error(f"File {target} does not exist!")
+        sys.exit(1)
+
+    raw_data = None
+
+    with open(target, "rb") as raw:
+        raw_data = raw.read()
+
+    log(f"Loading {target}...")
+
+    sprx: lief.Binary.FORMATS.ELF = lief.parse(raw_data)
+    symbol = sprx.get_symbol(DLL_START_NID)
+    
+    if not symbol:
+        log_error("Unable to find dll_start symbol!")
+        sys.exit(1)
+    
+    log("Found dll_start symbol, extracting PE...")
+
+    segment = sprx.segment_from_virtual_address(symbol.value)
+    
+    pe_addr = segment.virtual_address + (symbol.value - segment.virtual_address)
+    log(f"PE is at virtual address {hex(pe_addr)}, extracting...")
+    pe_content =  segment.content[(symbol.value - segment.virtual_address):].tobytes() # This is a memoryview object, not a list
+    
+    if not output:
+        if target.endswith(".dll.sprx"):
+            output = target[:-5]
+        else:
+            output = f"{target}.dll"
+    
+    log(f"Saving as {output}")
+    
+    with open(output, "wb") as out:
+        out.write(pe_content)
+    
+    log("Done!")
